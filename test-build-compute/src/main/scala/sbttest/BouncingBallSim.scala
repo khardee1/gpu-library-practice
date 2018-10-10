abaf9d089f5fdadb74b840428dee1b49fbba3e04
Kurt Hardee <khardee@trinity.edu>
2018-10-10 11:32:56 -0500

    bouncing off floor added and self-gravitation added

--- test-build-compute/src/main/scala/sbttest/BouncingBallSim.scala
+++ test-build-compute/src/main/scala/sbttest/BouncingBallSim.scala
@@ -3,8 +3,10 @@ package sbttest
 import com.thoughtworks.compute.gpu._
 
 object BouncingBallSim extends App {
-  val x = Tensor.fill(100f, Array(10))
+  val x = Tensor.fill(0f, Array(10))
+  val y = Tensor.fill(0f, Array(10))
+  val z = Tensor.fill(100f, Array(10))
   val v = Tensor.fill(10f, Array(10))
   val a = Tensor.fill(1f, Array(10))
-  BouncingBallWorld.runSim(x, v, a, 20f)
+  BouncingBallWorld.runSim(x, y, z, v, a, 20f)
 }
--- test-build-compute/src/main/scala/sbttest/BouncingBallWorld.scala
+++ test-build-compute/src/main/scala/sbttest/BouncingBallWorld.scala
@@ -4,16 +4,22 @@ import com.thoughtworks.compute.gpu._
 
 object BouncingBallWorld {
   
-  def runSim(x:Tensor, v:Tensor, a:Tensor, iter:Float):(Tensor, Tensor, Tensor, Float) = {
-    println(x.toString)
+  def runSim(x:Tensor, y:Tensor, z:Tensor, v:Tensor, a:Tensor, iter:Float, dt:Float):(Tensor, Tensor, Tensor, Tensor, Tensor, Float) = {
+    println(z.toString)
     println(v.toString)
     println(a.toString)
     println
     val newAcc = Tensor.fill(-1f, Array(10))
     val aPrime = a + newAcc
     val vPrime = v + a
-    val xPrime = x + v //*dt
+    val zPrime = z + v * Tensor(dt)
     
-    if(iter >= 0) runSim(xPrime,vPrime,aPrime,iter-1) else (xPrime,vPrime,aPrime,iter)
+    val boolZNegative = (Tensor.min(zPrime, Tensor(0f)) / zPrime) * Tensor(-1f)
+    val boolZPositive = (Tensor.max(zPrime, Tensor(0f)) / zPrime)
+    val boolZ = boolZNegative + boolZPositive
+    
+    val vFinal = vPrime * boolZ
+    
+    if(iter >= 0) runSim(x, y, zPrime, vFinal, aPrime, iter-1, dt) else (x, y, zPrime, vFinal, aPrime, iter, dt)
   }
 }
